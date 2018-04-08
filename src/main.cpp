a6fed3f7203ab8ef4c8bc541d3e9000adff94904
dpankratz <pankratz@ualberta.ca>
2018-04-08 10:03:05 -0600

    Added options file integration

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,38 @@
 #include <OgreFrameListener.h>
 #include <OgreRenderTarget.h>
 #include <OgreProfiler.h>
+#include <stdio.h>
 
 std::string usageText = "<appname> <-o or -d>\n-o is octree -d is default.\n";
 
 class MyTestApp : public OgreBites::ApplicationContext, public OgreBites::InputListener, Ogre::FrameListener
 {
 public:
-    MyTestApp(bool useBSP);
+    MyTestApp(bool useBSP,int runFrames,int rotationFrequency, float rotationConstant);
     void setup();
 	Ogre::SceneManager* create_scene();
     bool keyPressed(const OgreBites::KeyboardEvent& evt);
     bool frameEnded(const Ogre::FrameEvent& evt);
     void printFrameRateInfo(char* prefix);
+    void quit();
 private:
 	bool useOctree; // enable Octree culling optimization
     Ogre::Camera* cam; 
     Ogre::SceneNode *sn;
     Ogre::Vector3 camMoveDelta;
     int camTimesMoved;
+    int runFrames;
+    float rotationConstant;
+    int rotationFrequency;
 };
 
 //! [constructor]
-MyTestApp::MyTestApp(bool useOctree) : OgreBites::ApplicationContext("OgreTutorialApp")
+MyTestApp::MyTestApp(bool useOctree,int runFrames,int rotationFrequency,float rotationConstant) : OgreBites::ApplicationContext("OgreTutorialApp")
 {
 	this->useOctree = useOctree;
+    this->runFrames = runFrames;
+    this->rotationFrequency = rotationFrequency;
+    this->rotationConstant = rotationConstant;
 }
 //! [constructor]
 
@@ -39,8 +47,7 @@ bool MyTestApp::keyPressed(const OgreBites::KeyboardEvent& evt)
 {
     if (evt.keysym.sym == SDLK_ESCAPE)
     {
-        this->printFrameRateInfo("==FrameInfo==");
-        getRoot()->queueEndRendering();
+        this->quit();
     }
     return true;
 }
@@ -49,24 +56,30 @@ bool MyTestApp::keyPressed(const OgreBites::KeyboardEvent& evt)
 bool MyTestApp::frameEnded(const Ogre::FrameEvent& evt)
 {
     this->cam->setAutoTracking(true,sn);
-    this->cam->setPosition(cam->getPosition() + camMoveDelta);
+    this->cam->setPosition(cam->getPosition() + this->camMoveDelta);
 
-    switch(this->camTimesMoved){
-        case 30:
-            this->camMoveDelta = Ogre::Vector3(0,0,-5);
-            break;
-        case 60:
-            this->camMoveDelta = Ogre::Vector3(-5,0,0);
-            break;
-        case 90:
-            this->camMoveDelta = Ogre::Vector3(0,0,5);
-            break;
-        case 120:
-            this->camMoveDelta = Ogre::Vector3(5,0,0);
-            this->camTimesMoved = 0;
-            break;
+    if(this->camTimesMoved == this->rotationFrequency){        
+        this->camMoveDelta = Ogre::Vector3(0,0,-1) * this->rotationConstant;
+    }
+    else if(this->camTimesMoved == this->rotationFrequency * 2){        
+        this->camMoveDelta = Ogre::Vector3(-1,0,0)* this->rotationConstant;
+    }
+    else if(this->camTimesMoved == this->rotationFrequency * 3){        
+        this->camMoveDelta = Ogre::Vector3(0,0,1)* this->rotationConstant;
+    }
+    else if(this->camTimesMoved == this->rotationFrequency * 4){        
+        this->camMoveDelta = Ogre::Vector3(1,0,0)* this->rotationConstant;
+        this->camTimesMoved = 0;
     }
     this->camTimesMoved++;
+    if(runFrames != -1 && --runFrames == 0){
+        quit();
+    }
+}
+
+void MyTestApp::quit(){
+    this->printFrameRateInfo("==FrameInfo==");
+    getRoot()->queueEndRendering();
 }
 
 //! [setup]
@@ -111,7 +124,7 @@ void MyTestApp::setup(void)
     Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
     camNode->setPosition(0, 10, 27);
     camNode->lookAt(Ogre::Vector3(-10, 0, 0), Ogre::Node::TS_PARENT);
-    camMoveDelta = Ogre::Vector3(0.1f,0,0);
+    camMoveDelta = Ogre::Vector3(1,0,0) * this->rotationConstant;
     // create the camera
     cam = scnMgr->createCamera("myCam");
     cam->setNearClipDistance(5); // specific to this sample
@@ -141,6 +154,21 @@ void MyTestApp::printFrameRateInfo(char* prefix){
 int main(int argc, char *argv[])
 {
     bool useOctree = false;
+    FILE *options = fopen("./options","r");
+    int runFrames,rotationFrequency;
+    float rotationConstant;
+    if(options != NULL){
+        fscanf(options,"numFrames=%i\n",&runFrames);
+        fscanf(options,"rotationFrequency=%i\n",&rotationFrequency);
+        fscanf(options,"rotationConstant=%f\n",&rotationConstant);
+        std::cout << "Options:\n" << runFrames <<" , " << rotationFrequency << " , " << rotationConstant << "\n";
+    }
+    else{
+        std::cout << "No options file. Using default values";
+        runFrames = -1;
+        rotationFrequency = 15;
+        rotationConstant = 1;
+    }
     if(argc == 2)
     {
         if(strcmp(argv[1], "-o") == 0)
@@ -163,7 +191,8 @@ int main(int argc, char *argv[])
         std::cout << "Parameter format not recognized.\n" << usageText;
         return -1;
     }
-    MyTestApp app(useOctree);
+    
+    MyTestApp app(useOctree,runFrames,rotationFrequency,rotationConstant);
     app.initApp();
     app.getRoot()->startRendering();
     app.closeApp();
